fc93ab35eb3f940dd9c3afddfcef9f8965326dff
Vinicius Dias <dutrad.vinicius@gmail.com>
2022-05-29 19:21:14 +0100

    Added js code

---
+++ MMM-ArduinoTemp.js
@@ -0,0 +1,55 @@
+Module.register("MMM-ArduinoTemp", {
+	defaults: {
+		tempUnit: "C",
+    	freq: 60000,
+    	high: 80,
+    	low: 70,
+    	highColor: "red",
+    	lowColor: "green",
+    	otherColor: "yellow",
+		label: "Room:"
+	},
+	
+	start: function() {
+   		this.sendSocketNotification("get_Arduinotemp");
+  	},
+
+  	getDom: function() {
+   		 var e = document.createElement("div")
+   		 e.id = "arduino_temp"
+		return e
+	},
+
+ 	 notificationReceived: function(notification, payload, sender) {
+	 	switch(notification) {
+      			case "DOM_OBJECTS_CREATED":
+        		var timer = setInterval(()=>{
+				this.sendSocketNotification("get_Arduinotemp")
+        		}, this.config.freq)
+        		break
+    		}
+	},
+
+  	socketNotificationReceived: function(notification, payload) {
+		switch (notification) {
+			case "temperature":
+				var e = document.getElementById("arduino_temp");
+				if (parseFloat(payload) <= this.config.low) {
+					e.style.color = this.config.lowColor;
+				} else if (parseFloat(payload) >= this.config.high) {
+					e.style.color = this.config.highColor;
+				} else {
+					e.style.color = this.config.otherColor;
+				}
+
+				var temp;
+				if (this.config.tempUnit === "C") {
+					temp = payload.toString() + "°C";
+				} else {
+					temp = (payload * (9 / 5) + 32).toFixed(1).toString() + "°F";
+				}
+				e.innerHTML = this.config.label + " " + temp;
+				break;
+		}
+  	},
+})
---
+++ node_helper.js
@@ -0,0 +1,20 @@
+const spawn = require("child_process").spawn
+var NodeHelper = require("node_helper")
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function(notification, payload) {
+    switch(notification) {
+      case "get_Arduinotemp":
+        this.job()
+        break
+    }
+  },
+  
+  job: function() {
+    var process = spawn("python3", ["/home/pi/MagicMirror/modules/MMM-PiTemp/getTemp.py"])
+    process.stdout.on("data", (data)=>{
+      console.log(data)
+      this.sendSocketNotification("temperature", data.toString())
+    })
+  }
+})
